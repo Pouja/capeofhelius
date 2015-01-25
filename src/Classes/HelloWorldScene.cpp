157c4a74944053acae803e02872a375ac96efd67
PNikray <poujanik@gmail.com>
2015-01-25 17:24:44 +0100

    setviewpoint now works

--- src/Classes/HelloWorldScene.cpp
+++ src/Classes/HelloWorldScene.cpp
@@ -1,4 +1,5 @@
 #include "HelloWorldScene.h"
+
 USING_NS_CC;
 
 Scene* HelloWorld::createScene()
@@ -28,9 +29,39 @@ bool HelloWorld::init()
     std::string file = "tilemap.tmx";
     auto str = String::createWithContentsOfFile(FileUtils::getInstance()->fullPathForFilename(file.c_str()).c_str());
     this->tilemap = TMXTiledMap::createWithXML(str->getCString(),"");
-    // this->tilemap->getLayer("definitions")->setVisible(false);
-    this->background = this->tilemap->getLayer("bg");
     this->tilemap->setScale(2.0);
+
+    this->background = this->tilemap->getLayer("bg");
+    TMXObjectGroup* objectGroup = tilemap->getObjectGroup("objects");
+    ValueMap spawnPoint = objectGroup->getObject("spawnpoint");
+    int xSpawnPoint = spawnPoint.at("x").asInt() * tilemap->getScale();
+    int ySpawnPoint = spawnPoint.at("y").asInt() * tilemap->getScale();
+
     addChild(this->tilemap, -1);
+    this->setViewPointCenter(Vec2(xSpawnPoint,ySpawnPoint));
     return true;
+}
+
+void HelloWorld::setViewPointCenter(Vec2 position) {
+  Size winSize = Director::getInstance()->getWinSize();
+
+  Size mapSize = tilemap->getMapSize();
+  mapSize.width= mapSize.width * tilemap->getTileSize().width * tilemap->getScale();
+  mapSize.height= mapSize.height * tilemap->getTileSize().height * tilemap->getScale();
+
+  float xView = position.x - winSize.width/2;
+  if(position.x >= 0 && position.x <= winSize.width / 2) {
+    xView = 0;
+  } else if(mapSize.width - winSize.width/2 <= position.x && position.x <= mapSize.width) {
+    xView = mapSize.width - winSize.width;
+  }
+
+  float yView = position.x - winSize.width/2;;
+  if(position.y >= 0 && position.y <= winSize.height / 2) {
+    yView = 0;
+  } else if(mapSize.height - winSize.height/2 <= position.y && position.y <= mapSize.height) {
+    yView = mapSize.height - winSize.height;
+  }
+
+  this->setPosition(Vec2(-1*xView, yView));
 }
--- src/Classes/HelloWorldScene.h
+++ src/Classes/HelloWorldScene.h
@@ -3,6 +3,7 @@
 #define COCOS2D_DEBUG 1
 
 #include "cocos2d.h"
+#include "math/Vec2.h"
 
 class HelloWorld : public cocos2d::Layer
 {
@@ -15,6 +16,8 @@ public:
 
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
+
+    void setViewPointCenter(cocos2d::Vec2);
 
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
